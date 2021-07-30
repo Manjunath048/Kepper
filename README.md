6b7010ad5ab6b887ce7c27c72e47727d2d606411
Manjunath048 <manjunathgowda048@gmail.com>
2021-07-30 14:48:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jpptcl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jpptcl)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
